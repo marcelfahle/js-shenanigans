6da53fe22bbebfd0f48a4bbadb6292c7b4a6e217
Marcel Fahle <m.fahle@gmail.com>
2017-01-30 18:35:45 +0100

    adds describe block

--- src/closures/01-secret.test.js
+++ src/closures/01-secret.test.js
@@ -1,16 +1,17 @@
 import{ getSecret } from './01-secret';
 
 
-it('should not be accessible via object.', () => {
-  const obj = getSecret(1);
+describe('secret', () => {
+  it('should not be accessible via object.', () => {
+    const obj = getSecret(1);
+    expect(obj.secret).toBeFalsy(); 
+  });
 
-  expect(obj.secret).toBeFalsy(); 
-});
+  it('should be accessible via closure,', () => {
+    const expected = 1;
+    const obj = getSecret(1);
+    const actual = obj.get();
 
-it('should be accessible via closure,', () => {
-  const expected = 1;
-  const obj = getSecret(1);
-  const actual = obj.get();
-
-  expect(actual).toBe(expected);
+    expect(actual).toBe(expected);
+  });
 });

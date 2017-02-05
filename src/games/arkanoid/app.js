db24b6ae37d6a722b95745fba24385036c7adf9e
Marcel Fahle <m.fahle@gmail.com>
2017-02-05 10:16:32 +0100

    Arkanoid: lets ball reflect from paddle

--- src/games/arkanoid/app.js
+++ src/games/arkanoid/app.js
@@ -3,20 +3,35 @@ class App  {
     this.canvas = canvas;
     this.canvasContext = canvasContext;
 
-    this.famesPerSecond = 25;
+    this.famesPerSecond = 50;
     this.ballX = 75;
     this.ballY = 100;
     this.ballSpeedX = 5;
     this.ballSpeedY = 3;
 
-    this.drawCanvas();
-    
-    setInterval( this.drawAll.bind(this), 1000 / this.framesPerSecond );
-    this.drawAll();
+    this.paddleWidth = 100;
+    this.paddleThickness = 10;
+    this.paddleDistFromBottom = 60;
+    this.paddleX = 400;
+
+
+    setInterval( this.drawAll.bind(this),  1000/ this.famesPerSecond );
+
+    canvas.addEventListener( 'mousemove', this.updateMousePosition.bind( this ) );
   }
 
   getCanvas() {
     return this.canvas;
+  }
+
+  updateMousePosition( e ) {
+    const rect = this.canvas.getBoundingClientRect();
+    const root = document.documentElement;
+
+    const mouseX = e.clientX - rect.left - root.scrollLeft;
+    //const mouseY = e.clientY - rect.top - root.scrollTop;
+
+    this.paddleX = mouseX - this.paddleWidth / 2;
   }
 
 
@@ -24,10 +39,22 @@ class App  {
     this.drawCanvas();
     this.moveBall();
     this.drawBall();
+    this.colorRect( 
+      this.paddleX, 
+      this.canvas.height - ( this.paddleDistFromBottom ),
+      this.paddleWidth,
+      this.paddleThickness,
+      'white'
+    );
   }
 
   drawCanvas() {
     this.colorRect( 0,0, this.canvas.width,this.canvas.height, 'black' );
+  }
+
+  resetBall() {
+    this.ballX = this.canvas.width / 2;
+    this.ballY = this.canvas.height / 2;
   }
 
   moveBall() {
@@ -38,8 +65,21 @@ class App  {
     if ( this.ballX > canvas.width ) this.ballSpeedX *= -1; 
     if ( this.ballX < 0 ) this.ballSpeedX *= -1; 
 
-    if ( this.ballY > canvas.height ) this.ballSpeedY *= -1; 
+    if ( this.ballY > canvas.height ) this.resetBall();
     if ( this.ballY < 0 ) this.ballSpeedY *= -1; 
+
+    const paddleTopEdge = this.canvas.height - this.paddleDistFromBottom;
+    const paddleBottomEdge = paddleTopEdge + this.paddleThickness;
+
+    const paddleLeftEdge = this.paddleX;
+    const paddleRightEdge = paddleLeftEdge + this.paddleWidth;
+
+    if( this.ballY > paddleTopEdge &&
+        this.ballY < paddleBottomEdge &&
+        this.ballX > paddleLeftEdge &&
+        this.ballX < paddleRightEdge) {
+          this.ballSpeedY *= -1;        
+        }
   }
 
   drawBall() {
@@ -59,7 +99,6 @@ class App  {
     canvasContext.beginPath();
     canvasContext.arc( centerX,centerY, radius, 0,Math.PI*2, true );
     canvasContext.fill();
-    
   }
 
 }
